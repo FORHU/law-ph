7a5485aed4478c7f97d8abca5eac6600684edf16
joelralph <joelralph06@gmail.com>
2026-01-14 23:04:19 +0800

    fix: build issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { LandingPage } from "@/components/landing-page"
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import React from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/components/auth-provider";
