b9faf14c8ce12141761db27523f742346ff8301c
joelralph <joelralph06@gmail.com>
2026-05-28 01:55:56 +0800

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,14 +4,11 @@ import * as path from "path";
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 dotenv.config({ path: path.resolve(process.cwd(), ".env.local"), override: true });
 
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
   },
 });
